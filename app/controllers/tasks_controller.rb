bc0b08ac06cc3275a8664d16c2c152ba8329789f
Agota Kocsis <kogotti@yahoo.com>
2019-07-20 13:16:58 +0100

    test

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,2 +1,27 @@
 class TasksController < ApplicationController
+  def home
+  end
+
+  def index
+    @tasks = Task.all
+  end
+
+  def show
+  end
+
+  def new
+  end
+
+  def create
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,27 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+#For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get 'home', to: 'tasks#home'
+  # As a user, I can list tasks
+
+  get 'tasks', to: 'tasks#index'
+
+  # As a user, I can view the details of a task
+  get 'tasks/:id', to: 'tasks#show'
+
+  # As a user, I can add a new task
+
+  get 'tasks/new', to: 'tasks#new'
+  post 'tasks', to: 'tasks#create'
+
+  # As a user, I can edit a task
+  get 'tasks/:id/edit', to: 'tasks/edit'
+  patch 'tasks/:id', to: 'tasks#update'
+
+  # As a user, I can remove a task
+
+  delete 'tasks/:id', to: 'tasks#destroy'
+
 end
+
+
+
